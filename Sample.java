5b3df970b9ffb56be8c2d96b41a4a5bfa6849bfd
M Siddharth <124453253+SiddharthM458@users.noreply.github.com>
2023-09-15 09:45:24 +0530

    Create Sample.java

---
+++ Sample.java
@@ -0,0 +1,9 @@
+public class HelloGuys {
+  public static void main(String[]args){
+    System.out.println("Hey Vetri");
+    System.out.println("Hey Kumaran");
+    System.out.println("Hey Arun");
+    System.out.println("Hey Thamizh");
+  }
+}
+    
