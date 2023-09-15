9e375ec300a7d263450175d4946717526ebe0d10
M Siddharth <124453253+SiddharthM458@users.noreply.github.com>
2023-09-15 11:32:06 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -2,6 +2,9 @@ public class hello{
 	public static void main(String[] args){
 		System.out.println("Hey Siva");
 		System.out.println("Hey Vetri");
+		System.out.println("Hey Riya");
+		System.out.println("Hey Abinaya");
+		System.out.println("Hey Rahul");
 	}
 }
 		
