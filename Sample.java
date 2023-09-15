a002d0e60fe88f8abe0484ff71286ed3dafd5179
M Siddharth <124453253+SiddharthM458@users.noreply.github.com>
2023-09-15 11:35:06 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,10 +1,32 @@
-public class hello{
-	public static void main(String[] args){
-		System.out.println("Hey Siva");
-		System.out.println("Hey Vetri");
-		System.out.println("Hey Riya");
-		System.out.println("Hey Abinaya");
-		System.out.println("Hey Rahul");
+public class Method_Overloading {
+	
+	public void employee_details(String name) {
+		System.out.println("Employee Name :" + name);
 	}
+	
+	public void employee_details(int id) {
+		System.out.println("Employee ID is :" + id);
+	}
+	public void employee_details(long salary, float pf) {
+		System.out.println("Employee Salary is: " + " ," + "Employee Pf :" + pf);
+	}
+	
+	
+	public void employee_details(String name, int id) {
+		System.out.println("Employee Name :" + name + " , " + "Employee id :" + id);
+	}
+	
+	public void employee_details(int salary, int pf ) {
+		System.out.println("Employee salary :" + salary + " , " + "Employee pf :" + pf);
+	}
+	
+	public static void main(String[] args) {
+		Method_Overloading mol = new Method_Overloading();
+		mol.employee_details("Siddharth");
+		mol.employee_details(2216071);
+		mol.employee_details(45356, 14538);
+		mol.employee_details("Vetrivel", 2021567);
+		mol.employee_details(35000, 400691);
+	}
+
 }
-		
